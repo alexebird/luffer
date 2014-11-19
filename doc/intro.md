243d3be428c2641450a51895d4fb049c9fd9e12e
Alex Bird <alexebird@gmail.com>
2014-11-18 22:19:47 -0800

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to luffer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/luffer/core_test.clj
@@ -0,0 +1,7 @@
+(ns luffer.core-test
+  (:require [clojure.test :refer :all]
+            [luffer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
