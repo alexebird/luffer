244de8b7c83407ac012624fff76e97d1f3d9e160
Alex Bird <bird@grandrounds.com>
2016-01-07 09:55:11 -0800

    flesh out differences between ruby to indexed json fields and luffer fields

--- src/luffer/models.clj
+++ src/luffer/models.clj
@@ -15,98 +15,51 @@
 
 (declare plays users tracks shows tours venues api_clients)
 
-;; play model
-;t.integer  "user_id"
-;t.integer  "track_id"
-;t.integer  "api_client_id"
-;t.integer  "play_event_id"
-;t.text     "original_play_data"
-;t.string   "source"
-;t.string   "ip_address"
-;t.datetime "created_at"
-;t.datetime "updated_at"
 (korma.core/defentity plays
-  (korma.core/entity-fields :id :user_id :track_id :api_client_id :source :created_at)
+  (korma.core/entity-fields :id :source :created_at :ip_address :play_event_id)
   (korma.core/belongs-to users {:fk :user_id})
   (korma.core/belongs-to tracks {:fk :track_id})
   (korma.core/belongs-to api_clients {:fk :api_client_id}))
 
-
-;; user model
-;t.string   "username"
-;t.string   "email"
-;t.datetime "created_at"
 (korma.core/defentity users
   (korma.core/entity-fields :id :username :email :created_at)
   (korma.core/has-many plays))
 
-
-;; track model
-;t.integer  "show_id"
-;t.integer  "remote_id"
-;t.string   "title"
-;t.string   "slug"
-;t.integer  "position"
-;t.decimal  "position_in_set"
-;t.decimal  "position_in_show"
-;t.integer  "duration"
-;t.string   "set"
-;t.string   "set_name"
-;t.integer  "set_index"
-;t.string   "mp3"
-;t.string   "unique_slug"
-;t.string   "source"
-;t.datetime "created_at"
 (korma.core/defentity tracks
-  (korma.core/entity-fields :id :show_id :remote_id :title :slug :position :position_in_set :position_in_show :duration :set :set_name :set_index :mp3 :unique_slug :source :created_at)
+  (korma.core/entity-fields :id :remote_id :title :slug :position
+                            :position_in_set :position_in_show :duration :set
+                            :set_name :set_index :mp3 :unique_slug :source :created_at)
+  ; :string_id => :unique_slug
+  ; :set_data [
+  ;   :string_id => :show.date$:set_name
+  ;   :id => :show.id$:set_name
+  ;   :index => :set_index
+  ;   :name => :set
+  ;   :full_name => :set_name
+  ;  ]
   (korma.core/has-many plays)
   (korma.core/belongs-to shows {:fk :show_id}))
 
-;; show model
-;t.integer  "tour_id"
-;t.integer  "venue_id"
-;t.integer  "remote_id",  :null => false
-;t.string   "date",       :null => false
-;t.integer  "duration"
-;t.boolean  "sbd"
-;t.boolean  "remastered"
-;t.string   "source",     :null => false
 (korma.core/defentity shows
-  (korma.core/entity-fields :id :tour_id :venue_id :remote_id :date :duration :sbd :remastered :source)
+  (korma.core/entity-fields :id :remote_id :date :duration :sbd :remastered :source :year :era)
+  ; :string_id => :date
+  ; :year => :date substring
+  ; :era => some kind of mapping
   (korma.core/has-many tracks)
   (korma.core/belongs-to tours {:fk :tour_id})
   (korma.core/belongs-to venues {:fk :venue_id}))
 
-
-;; tour model
-;t.integer  "remote_id",   :null => false
-;t.string   "name",        :null => false
-;t.integer  "shows_count"
-;t.string   "slug",        :null => false
-;t.string   "starts_on"
-;t.string   "ends_on"
-;t.string   "source",      :null => false
 (korma.core/defentity tours
-  (korma.core/entity-fields :id :remote_id :name :shows_count :slug :starts_on :ends_on :source)
+  (korma.core/entity-fields :id :name :slug :starts_on :ends_on :source)
+  ; :string_id => :slug
   (korma.core/has-many shows))
 
-
-;; venue model
-;t.integer  "remote_id",   :null => false
-;t.string   "name",        :null => false
-;t.text     "past_names"
-;t.decimal  "latitude"
-;t.decimal  "longitude"
-;t.integer  "shows_count"
-;t.string   "location",    :null => false
-;t.string   "slug",        :null => false
-;t.string   "source",      :null => false
 (korma.core/defentity venues
-  (korma.core/entity-fields :id :remote_id :name :past_names :latitude :longitude :shows_count :location :slug :source)
+  (korma.core/entity-fields :id :name :past_names :location :slug :source)
+  ; :string_id => :slug
+  ; :location_point [:lat :lon] => {:latitude.to_f :longitude.to_f}
   (korma.core/has-many shows))
 
-
-;; api_client model
 (korma.core/defentity api_clients
   (korma.core/entity-fields :id :name :description)
   (korma.core/has-many plays)
