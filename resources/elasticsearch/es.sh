1cda8d695554a94ceb372d64097be0b9e45454ae
Alex Bird <bird@grandrounds.com>
2016-01-08 00:22:28 -0800

    add helper functoins

--- resources/elasticsearch/es.sh
+++ resources/elasticsearch/es.sh
@@ -14,11 +14,11 @@ put_plays_template() {
 put_cluster_settings() {
   curl "${ES_HOST}/_cluster/settings" -XPUT -d@- <<-HERE | jq '.'
 {
-    "persistent" : {
-        "indices.store.throttle.max_bytes_per_sec" : "200mb",
-	"indices.store.throttle.type" : "merge",
-	"bootp.mlockall": true
-    }
+  "persistent": {
+    "indices.store.throttle.max_bytes_per_sec": "200mb",
+    "indices.store.throttle.type": "merge",
+    "bootp.mlockall": true
+  }
 }
 HERE
 }
@@ -26,9 +26,9 @@ HERE
 disable_cluster_settings_for_fast_index() {
   curl "${ES_HOST}/_cluster/settings" -XPUT -d@- <<-HERE | jq '.'
 {
-    "transient" : {
-	"indices.store.throttle.type" : "merge"
-    }
+  "transient": {
+    "indices.store.throttle.type": "merge"
+  }
 }
 HERE
 }
@@ -36,16 +36,76 @@ HERE
 enable_cluster_settings_for_fast_index() {
   curl "${ES_HOST}/_cluster/settings" -XPUT -d@- <<-HERE | jq '.'
 {
-    "transient" : {
-	"indices.store.throttle.type" : "none"
-    }
+  "transient": {
+    "indices.store.throttle.type": "none"
+  }
 }
 HERE
 }
 
-main() {
-  check_required_env
-  eval "${1}"
+remove_plays_alias()
+{
+  local idx_name="${1}"
+  local plays_alias_name='plays'
+  curl -s "${ES_HOST}/_aliases" -XPOST -d@- <<-HERE | jq '.'
+{
+  "actions": [
+    { "remove": { "index": "${idx_name}", "alias" : "${plays_alias_name}" } }
+  ]
+}
+HERE
+
+  aliases
 }
 
-main "${1}"
+add_plays_alias()
+{
+  local idx_name="${1}"
+  local plays_alias_name='plays'
+  curl -s "${ES_HOST}/_aliases" -XPOST -d@- <<-HERE | jq '.'
+{
+  "actions": [
+    { "add": { "index": "${idx_name}", "alias" : "${plays_alias_name}" } }
+  ]
+}
+HERE
+
+  aliases
+}
+
+mk_index()
+{
+  local idx_name="${1}"
+  curl -s "${ES_HOST}/${idx_name}" -XPUT | jq '.'
+  indices
+}
+
+rm_index()
+{
+  local idx_name="${1}"
+  [[ "$2" == 'xXx' ]] || { echo must pass magic ; exit 1 ; }
+  curl -s "${ES_HOST}/${idx_name}" -XDELETE | jq '.'
+  indices
+}
+
+aliases()
+{
+  curl -s ${ES_HOST}/_cat/aliases?v
+}
+
+health()
+{
+  curl -s ${ES_HOST}/_cat/health?v
+}
+
+indices()
+{
+  curl -s ${ES_HOST}/_cat/indices?v
+}
+
+main() {
+  check_required_env
+  eval "${@}"
+}
+
+main "${@}"
